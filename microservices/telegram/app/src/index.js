b9e6003977e5f00e8c888675c74873606706902a
choonfahh <choonfahh@gmail.com>
2018-03-20 14:49:12 +0800

    Test #3

--- microservices/telegram/app/src/index.js
+++ microservices/telegram/app/src/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const app = express()
+const Telegraf = require(`telegraf`);
+const Markup = require(`telegraf/markup`);
+const Extra = require(`telegraf/extra`);
+const Composer = require(`telegraf/composer`);
+const session = require(`telegraf/session`);
+const Stage = require(`telegraf/stage`);
+const WizardScene = require(`telegraf/scenes/wizard`);
+const Scene = require("telegraf/scenes/base");
+const { mount } = require("telegraf");
+const { enter, leave } = Stage;
 
 app.get('/', (req, res) => res.send('Share A Song Telegram Bot'))
 
